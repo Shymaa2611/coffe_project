730e8aabbbf228127c889f409ae433e62133ecd6
Your Name <you@example.com>
2023-06-25 10:42:30 +0200

    addfunctions and urls for each page in project

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+    path('blog',views.blog,name='blog'),
+    path('coffes',views.coffes,name='coffes'),
+    path('contact',views.contact,name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 def index(request):
-    return render(request,'pages/index.html')
+    return render(request,"pages/index.html")
+def about(request):
+    return render(request,"pages/about.html")
+def blog(request):
+    return render(request,"pages/blog.html")
+def contact(request):
+    return render(request,"pages/contact.html")
+def coffes(request):
+    return render(request,"pages/coffes.html")
